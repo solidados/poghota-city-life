2df201018540e367a85af67f04e4d1d009fbc842
solidados <geekbrains.pk@internet.ru>
2023-11-26 18:01:42 +0400

    feat: setup server to return data on HomePage

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,6 +1,28 @@
+import {useEffect, useState} from "react";
+
 const Home = () => {
+  const [complaints, setComplaints] = useState(null)
+
+  useEffect(() => {
+    const fetchComplaints = async () => {
+      const response = await fetch('/api/complaints');
+      const json = await response.json();
+
+      if (response.ok) {
+        setComplaints(json)
+      }
+    }
+
+    fetchComplaints().catch((e) => console.error(e.message))
+  }, []);
   return (
-    <div>Home</div>
+    <div className="home">
+      <div className="complaints">
+        {complaints && complaints.map((complaint) => (
+          <p key={complaint._id}>{complaint.title}</p>
+        ))}
+      </div>
+    </div>
   );
 };
 
