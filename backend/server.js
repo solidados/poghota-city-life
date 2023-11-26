857fc4ab1e7129c4dc342cf8ff74dd2bce99278e
solidados <geekbrains.pk@internet.ru>
2023-11-26 14:51:02 +0400

    chore: setup mongoose and mongoDB connection

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,9 @@
 require('dotenv').config()
 
 const express = require('express')
+const mongoose = require('mongoose')
 const complaintRoutes = require('./routes/complaints')
+
 
 // * express app
 const app = express()
@@ -14,10 +16,15 @@ app.use((req, res, next) => {
   next()
 })
 
-// * routes
+// * routes (setting up a route listener)
 app.use('/api/complaints/', complaintRoutes)
 
-// * listening requests
-app.listen(process.env.PORT, () => {
-  console.log(`listening on port: ${process.env.PORT}`)
-})
+// * connect to DB via mongoose
+mongoose.connect(process.env.MONGO_URI)
+        .then(() => {
+          // listen for requests
+          app.listen(process.env.PORT, () => {
+            console.log(`connected to DB and listening on port ${process.env.PORT}`)
+          })
+        })
+        .catch((error) => console.error(error.message))
