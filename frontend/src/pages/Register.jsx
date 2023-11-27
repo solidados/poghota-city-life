2512931e1282c32b896bf24373a321de52894657
solidados <geekbrains.pk@internet.ru>
2023-11-27 15:49:29 +0400

    feat: add useRegister hook to use it in Register component

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,12 +1,15 @@
 import { useState } from "react";
+import { useRegister } from '../hooks/useRegister'
 
 const Register = () => {
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
+  const { register, isLoading, error } = useRegister()
 
   const handleSubmit = async (e) => {
     e.preventDefault()
-    console.log(email, password)
+
+    await register(email, password)
   }
 
   return (
@@ -27,7 +30,8 @@ const Register = () => {
           value={password}
         />
       </label>
-      <button>Register</button>
+      <button disabled={isLoading}>Register</button>
+      {error && <div className="error">{error}</div>}
     </form>
   );
 };
