15a6f8a1e49580c02a1a3f489ff08a6e407705e8
solidados <geekbrains.pk@internet.ru>
2023-11-27 17:13:31 +0400

    feat: add useEffect to AuthContext to update component only if the user is saved in localStorage and logged in

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,10 +1,12 @@
 import { Link } from "react-router-dom";
 import { useLogout } from "../hooks/useLogout";
+import { useAuthContext } from "../hooks/useAuthContext";
 
 import logo from "../assets/logo.png";
 
 const Navbar = () => {
   const { logout } = useLogout()
+  const { user } = useAuthContext()
 
   const handleClick = () => {
     logout()
@@ -17,13 +19,18 @@ const Navbar = () => {
           <img className="navbar-logo" src={logo} alt="logo" />
         </Link>
         <nav>
-          <div>
-            <button onClick={handleClick}>Logout</button>
-          </div>
-          <div>
-            <Link to="/login">Login</Link>
-            <Link to="/register">Register</Link>
-          </div>
+          {user && (
+            <div>
+              <span>{user.email}</span>
+              <button onClick={handleClick}>Logout</button>
+            </div>
+          )}
+          {!user && (
+            <div>
+              <Link to="/login">Login</Link>
+              <Link to="/register">Register</Link>
+            </div>
+          )}
         </nav>
       </div>
     </header>
