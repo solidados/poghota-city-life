ee3d6f26774171b5aa08f462628acf0a737a9c8b
lucy <lusearzumanyan99@gmail.com>
2023-11-28 16:34:25 +0400

    feat: implement user registration and login functionality

--- auth.py
+++ auth.py
@@ -6,7 +6,6 @@ from werkzeug.security import generate_password_hash, check_password_hash
 app = Flask(__name__)
 CORS(app)
 client = MongoClient("mongodb://localhost:27017/")
-
 db = client["user_auth"]
 users_collection = db["users"]
 
