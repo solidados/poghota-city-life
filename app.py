6e1d453b527f1027317476287f914a7fccbbc655
lucy <lusearzumanyan99@gmail.com>
2023-11-29 10:55:55 +0400

    feat: implement user registration, login, add complaint functionality

--- app.py
+++ app.py
@@ -1,19 +1,25 @@
+#import logging
+import jwt
+from datetime import datetime, timedelta
+from bson import ObjectId
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 from pymongo import MongoClient
 from werkzeug.security import generate_password_hash, check_password_hash
-from pymongo.errors import ConnectionFailure
+# from pymongo.errors import ConnectionFailure
 
 app = Flask(__name__)
 CORS(app)
+CORS(app, resources={r"/complaints": {"origins": "*", "methods": ["GET", "POST"]}})
 client = MongoClient("mongodb://localhost:27017/")
 
-try:
-    client.admin.command('ping')
-except ConnectionFailure:
-    print("Server not available")
-db = client["user_auth"]
+# try:
+#     client.admin.command('ping')
+# except ConnectionFailure:
+#     print("Server not available")
+db = client["poghota"]
 users_collection = db["users"]
+complaints_collection = db["complaints"]
 
 
 @app.route('/register', methods=['POST'])
@@ -56,11 +62,40 @@ def login_user():
     user = users_collection.find_one({"email": email})
 
     if user and check_password_hash(user['password'], password):
-        return jsonify({"message": "Login successful"}), 200
+        expiration = datetime.utcnow() + timedelta(days=1)
+        # needs to be changed later
+        token = jwt.encode({'email': email, 'exp': expiration}, 'secret-key', algorithm='HS256')
+        # return jsonify({"message": "Login successful"}), 200
+        # print(f"Generated Token: {token}")
+        return jsonify({"message": "Login successful", "token": token}), 200
     else:
         return jsonify({"error": "Invalid email or password"}), 401
 
 
-if __name__ == '__main__':
+@app.route('/complaints', methods=['POST'])
+def add_complaint():
 
+    # complaints_collection = db["complaints"]
+    #
+    # if "complaints" not in db.list_collection_names():
+    #     db.create_collection("complaints")
+
+    data = request.get_json()
+    title = data.get('title')
+    department = data.get('department')
+    location = data.get('location')
+    description = data.get('description')
+
+    complaint_data = {
+        "title": title,
+        "department": department,
+        "location": location,
+        "description": description
+    }
+
+    result = complaints_collection.insert_one(complaint_data)
+    return jsonify({"message": "Complaint added successfully", "complaint_id": str(result.inserted_id)}), 201
+
+
+if __name__ == '__main__':
     app.run(debug=True)
