c6c258548ed137934c1e4f376b4f6ec35bbee48b
solidados <geekbrains.pk@internet.ru>
2023-12-18 14:59:32 +0400

    feat: add styles for Contact Page background

--- frontend/src/pages/Contact/TeamPage.jsx
+++ frontend/src/pages/Contact/TeamPage.jsx
@@ -2,20 +2,24 @@ import TeamMateCard from "../../components/TeamMateCard";
 import { teamMates } from "../../data/TeamMates.js";
 
 import './teamPage.css';
+import MainBackground from "../../components/background/MainBackground";
 
 function TeamPage () {
 
   return (
-    <div className="team-page">
-      <div className="team-container">
-        <h2 className="team-title">Our Team</h2>
-        <div className="team">
-          {teamMates.map((teamMate) => (
-            <TeamMateCard key={teamMate.id} teamMate={teamMate} />
-          ))}
+    <>
+      <MainBackground />
+      <div className="team-page">
+        <div className="team-container">
+          <h2 className="team-title">Our Team</h2>
+          <div className="team">
+            {teamMates.map((teamMate) => (
+              <TeamMateCard key={teamMate.id} teamMate={teamMate} />
+            ))}
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
 
